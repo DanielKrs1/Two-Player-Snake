25be5d89c3a7c2d8bfb50950db8762fa16b66f96
jadenmanuel <jadenmanuel2006@gmail.com>
2023-06-16 15:14:06 -0400

    Map 1 complete
    Pretty sure its finished

---
+++ Two Player Snake/Assets/Scripts/MapCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreate : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public Camera mainCamera;
+
+    //public GameObject tileSprite;
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        
+        Map();
+    }
+
+    void Map()
+    {
+        GameObject newTile = Instantiate(Resources.Load("Square") as GameObject, new Vector3(0,0), Quaternion.identity);
+
+        newTile.transform.localScale = new Vector3(width, height, 1);
+
+        mainCamera.orthographicSize = newTile.transform.localScale.y/2 + 1;
+    }
+}
