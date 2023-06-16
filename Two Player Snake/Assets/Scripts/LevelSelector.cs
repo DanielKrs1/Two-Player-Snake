9f7620f33b5a68f13b7cd0d7879a65dab0769f8a
DanielKrs1 <krstndaniel24@gmail.com>
2023-06-16 18:32:54 -0400

    cleaned jaden's code a little

--- Two Player Snake/Assets/Scripts/LevelSelector.cs
+++ Two Player Snake/Assets/Scripts/LevelSelector.cs
@@ -1,24 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelSelector : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void LevelSelect(string toLevel)
-    {
+public class LevelSelector : MonoBehaviour {
+    public void LevelSelect(string toLevel) {
         SceneManager.LoadScene(toLevel);
     }
 }
--- Two Player Snake/Assets/Scripts/MapCreate.cs
+++ Two Player Snake/Assets/Scripts/MapCreate.cs
@@ -1,28 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MapCreate : MonoBehaviour
-{
+public class MapCreate : MonoBehaviour {
     public int width;
     public int height;
-    public Camera mainCamera;
 
-    //public GameObject tileSprite;
-
-    // Start is called before the first frame update.
-    void Start()
-    {
-        
-        Map();
-    }
-
-    void Map()
-    {
-        GameObject newTile = Instantiate(Resources.Load("Square") as GameObject, new Vector3(0,0), Quaternion.identity);
+    private void Start() {
+        GameObject newTile = Instantiate(Resources.Load("Square") as GameObject, new Vector3(0, 0), Quaternion.identity);
 
         newTile.transform.localScale = new Vector3(width, height, 1);
 
-        mainCamera.orthographicSize = newTile.transform.localScale.y/2 + 1;
+        Camera.main.orthographicSize = newTile.transform.localScale.y / 2 + 1;
     }
 }
