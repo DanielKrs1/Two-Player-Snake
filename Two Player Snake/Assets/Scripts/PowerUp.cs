3a3b473280facbe0f4b2b5571bc391ed3242d46f
Christopher Rodriguez <ckrod16@gmail.com>
2023-06-17 12:38:42 -0400

    Apple powerup is done

--- Two Player Snake/Assets/Scripts/PowerUp.cs
+++ Two Player Snake/Assets/Scripts/PowerUp.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public abstract class PowerUp : MonoBehaviour {
     public GameObject prefab;
 
-    private void OnCollisionEnter(Collision collision) {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("here");
         if (collision.gameObject.TryGetComponent(out Snake snake)) {
             OnCollect(snake);
             Destroy(gameObject);
@@ -12,13 +13,14 @@ public abstract class PowerUp : MonoBehaviour {
 
     public abstract void OnCollect(Snake snake);
 
-    public static void SpawnNewPowerUp() {
-        Vector2 spawnPosition = Map.instance.GetRandomPosition();
+    public void SpawnNewPowerUp() {
+        // Vector2 spawnPosition = Map.instance.GetRandomPosition();
 
-        while (Physics2D.OverlapPoint(spawnPosition) != null) {
-            spawnPosition = Map.instance.GetRandomPosition();
-        }
-
+        // while (Physics2D.OverlapPoint(spawnPosition) != null) {
+        //     spawnPosition = Map.instance.GetRandomPosition();
+        // }
+        Vector2 spawnPosition = new Vector2(Random.Range(-8.0f, 8.0f), Random.Range(8.0f, 8.0f));
+        Instantiate(prefab, new Vector3(spawnPosition.x, spawnPosition.y), Quaternion.identity);
         //do something to spawn powerup, maybe create probabilities so one is more common than another
     }
 }
