7907553d42e65c848fc1de38304b93d17f1a1a04
DanielKrs1 <krstndaniel24@gmail.com>
2023-06-17 11:30:15 -0400

    e

--- Two Player Snake/Assets/Scripts/Map.cs
+++ Two Player Snake/Assets/Scripts/Map.cs
@@ -12,16 +12,19 @@ public class Map : MonoBehaviour {
         GameObject newTile = Instantiate(Resources.Load("Square") as GameObject, new Vector3(0, 0), Quaternion.identity);
         newTile.transform.localScale = new Vector3(size.x, size.y, 1);
         Camera.main.orthographicSize = newTile.transform.localScale.y / 2 + 1;
+
+        Instantiate(redSnake, new Vector3(redSnakeSpawn.x, redSnakeSpawn.y), Quaternion.identity);
+        Instantiate(blueSnake, new Vector3(blueSnakeSpawn.x, blueSnakeSpawn.y), Quaternion.identity);
     }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(Vector3.zero, new Vector3(size.x, size.y));
 
-        Gizmos.color = Color.blue;
+        Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(new Vector3(redSnakeSpawn.x, redSnakeSpawn.y), 0.5f);
 
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(new Vector3(blueSnakeSpawn.x, blueSnakeSpawn.y), 0.5f);
     }
 }
