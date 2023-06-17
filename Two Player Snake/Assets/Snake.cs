7ec3b7d4092ebab253d102f3d2dc159952206579
zeypen24 <71848482+zeypen24@users.noreply.github.com>
2023-06-16 22:01:41 -0400

    Vector2int fix
    okay we are pushing

--- Two Player Snake/Assets/Snake.cs
+++ Two Player Snake/Assets/Snake.cs
@@ -2,30 +2,34 @@
 
 public class Snake : MonoBehaviour
 {
-    private Vector2 _direction = Vector2.right;
+    private Vector2int _direction = Vector2int.right;
 
     private void Update()
     { 
         if (Input.GetKeyDown(KeyCode.W)){
-            _direction = Vector2.up;
+            _direction = Vector2int.up;
         }
         else if (Input.GetKeyDown(KeyCode.S)){
-            _direction = Vector2.down;
+            _direction = Vector2int.down;
         }
         else if (Input.GetKeyDown(KeyCode.A)){
-            _direction = Vector2.left;
+            _direction = Vector2int.left;
         }
         else if (Input.GetKeyDown(KeyCode.D)){
-            _direction = Vector2.right;
+            _direction = Vector2int.right;
         }
     }
 
     private void FixedUpdate()
     {
-        this.transform.position = new Vector3(
+        this.transform.position = new Vector3int(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
+
             0.0f
         );
     }
+
+
+
 }
