c6eaf4b1c86f5ce50b93e3cd019a6e611ccfc1d1
DanielKrs1 <krstndaniel24@gmail.com>
2023-06-18 09:36:44 -0400

    power up sprites

---
+++ Two Player Snake/Assets/Scripts/Map/StartCountdown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class StartCountdown : MonoBehaviour {
+    [SerializeField] private GameObject countdownScreen;
+    [SerializeField] private TextMeshProUGUI countdownText;
+
+    private void Start() {
+        Time.timeScale = 0f;
+        StartCoroutine(Count());
+    }
+
+    private IEnumerator Count() {
+        int i = 3;
+
+        while (i > 0) {
+            countdownText.text = i.ToString();
+            i--;
+            yield return new WaitForSecondsRealtime(1f);
+        }
+
+        countdownScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
